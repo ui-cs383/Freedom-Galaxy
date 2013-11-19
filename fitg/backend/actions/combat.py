f645ebc4ddf293433947d22e7a719a7e5a5b6d3d
Jeff Crocker <crockj72@gmail.com>
2013-11-19 12:28:29 -0800

    Actions modified

--- fitg/backend/actions/combat.py
+++ fitg/backend/actions/combat.py
@@ -5,94 +5,118 @@ from random import randint
 
 def char_combat(atk_id, def_id, options):
 
-	session = Session()
+    session = Session()
 
-	atk_stack = session.query(Stack).filter_by(id = atk_id).one()
-	def_stack = session.query(Stack).filter_by(id = def_id).one()
-	session.add(atk_stack, def_stack)
+    atk_stack = session.query(Stack).filter_by(id = atk_id).one()
+    def_stack = session.query(Stack).filter_by(id = def_id).one()
+    session.add(atk_stack, def_stack)
 
-	# still needs to include breakoff and capture modifiers
-	# also decision if firefight or hand-to-hand
+    # still needs to include breakoff
 
-	CD = char_combat_rating(atk_stack, session) - char_combat_rating(def_stack, session)
 
-	if 'C' in options:
-		CD -= 2
+    CD = char_combat_rating(atk_stack, session) - char_combat_rating(def_stack, session)
 
-	atk_result = char_table(randint(0,5),CD,True)
-	def_result = char_table(randint(0,5),CD,False)
+    if 'C' in options:
+        CD -= 2
 
-	if 'F' in options:
-		atk_result[0] *= 2
-		def_result[0] *= 2
+    atk_result = char_table(randint(0,5),CD,True)
+    def_result = char_table(randint(0,5),CD,False)
 
-	elif atk_stack.militaryunits:
-		atk_result[0] *= 2
-		def_result[0] *= 2
 
-	char_wounds(atk_stack, def_result[0], session)
-	char_wounds(def_stack, atk_result[0], session)
-		
-	if 'C' in options:
-		if atk_result[1] == 1:
-			captured_char = def_stack.characters[randint(0,len(def_stack.characters)-1)]
-			captured_char.stack_id = atk_id
-			captured_char.captive = True
-			captured_char.active = False
+    if 'F' in options:
+        atk_result[0] *= 2
+        def_result[0] *= 2
 
-		if def_result[1] == 1:
-			captured_char = def_stack.characters[randint(0,len(def_stack.characters)-1)]
-			captured_char.stack_id = atk_id
-			captured_char.captive = True
-			captured_char.active = False
+    elif atk_stack.militaryunits:
+        atk_result[0] *= 2
+        def_result[0] *= 2
 
-	if atk_stack.size() == 0:
-		session.delete(atk_stack)
+    char_wounds(atk_stack, atk_result[0], session)
+    char_wounds(def_stack, def_result[0], session)
+        
+    if 'C' in options:
+        if atk_result[1] == 1:
+            captured_char = def_stack.characters[randint(0,len(def_stack.characters)-1)]
+            captured_char.stack_id = atk_id
+            captured_char.captive = True
+            captured_char.active = False
 
-	if def_stack.size() == 0:
-		session.delete(def_stack)
+        if def_result[1] == 1:
+            captured_char = def_stack.characters[randint(0,len(def_stack.characters)-1)]
+            captured_char.stack_id = atk_id
+            captured_char.captive = True
+            captured_char.active = False
 
-	session.commit()
+    if atk_stack.size() == 0:
+        session.delete(atk_stack)
+
+    if def_stack.size() == 0:
+        session.delete(def_stack)
+
+    session.commit()
 
 def char_wounds(stack, num_wounds, session):
-	while num_wounds > 0:
-		victim = stack.characters[randint(0,len(stack.characters)-1)]
-		session.add(victim)
-		victim.wounds += 1
-		if victim.wounds >= victim.endurance:
-			session.delete(victim)
-		num_wounds -= 1
-	session.commit()
+    while num_wounds > 0 and stack.characters:
+        victim = stack.characters[randint(0,len(stack.characters)-1)]
+        session.add(victim)
+        victim.wounds += 1
+        print victim.name, " suffers wound!"
+        if victim.wounds >= victim.endurance:
+            print victim.name, " has died! :("
+            session.delete(victim)
+        num_wounds -= 1
 
+    session.commit()
 
 def char_combat_rating(stack, session):
-	CR = 0
-	for character in stack.characters:
-		if character.active == True:
-			CR += character.combat - character.wounds
-	return CR
+    CR = 0
+    for character in stack.characters:
+        if character.active == True:
+            CR += character.combat - character.wounds
+    return CR
 
 def char_table(dice, CD, is_attacker):
-	attacker_wounds = (
-		(4,3,3,2,2,2,2,1,1,1),(4,3,2,2,2,1,1,1,1,0),(3,3,2,2,1,1,1,1,0,0),
-		(3,2,2,1,1,1,0,0,0,0),(2,2,1,1,0,0,0,0,0,0),(2,1,0,0,0,0,0,0,0,0))
+    attacker_wounds = (
+        (4,3,3,2,2,2,2,1,1,1),(4,3,2,2,2,1,1,1,1,0),(3,3,2,2,1,1,1,1,0,0),
+        (3,2,2,1,1,1,0,0,0,0),(2,2,1,1,0,0,0,0,0,0),(2,1,0,0,0,0,0,0,0,0))
 
-	defender_wounds = (
-		(0,0,0,0,0,0,0,1,1,2),(0,0,0,0,0,0,1,1,1,2),(0,0,0,0,0,1,1,1,2,3),
-		(0,0,0,1,1,1,1,2,3,3),(0,0,1,1,1,1,2,2,3,4),(1,1,1,1,2,2,2,3,3,4))
+    defender_wounds = (
+        (0,0,0,0,0,0,0,1,1,2),(0,0,0,0,0,0,1,1,1,2),(0,0,0,0,0,1,1,1,2,3),
+        (0,0,0,1,1,1,1,2,3,3),(0,0,1,1,1,1,2,2,3,4),(1,1,1,1,2,2,2,3,3,4))
 
-	attacker_capture = (
-		(0,0,0,0,0,1,0,0,0,0),(0,0,0,1,1,0,1,1,1,0),(0,1,0,0,0,0,0,0,0,0),
-		(0,0,1,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,1,0,0),(0,0,0,0,0,0,0,0,0,0))
+    attacker_capture = (
+        (0,0,0,0,0,1,0,0,0,0),(0,0,0,1,1,0,1,1,1,0),(0,1,0,0,0,0,0,0,0,0),
+        (0,0,1,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,1,0,0),(0,0,0,0,0,0,0,0,0,0))
 
-	defender_capture = (
-		(0,0,0,0,0,0,0,0,1,0),(0,0,0,0,0,0,0,0,0,0),(0,0,0,0,1,0,0,1,1,0),
-		(0,0,0,1,0,1,1,1,0,0),(0,0,0,0,0,0,1,0,1,0),(0,0,0,0,0,0,0,0,0,0))
+    defender_capture = (
+        (0,0,0,0,0,0,0,0,1,0),(0,0,0,0,0,0,0,0,0,0),(0,0,0,0,1,0,0,1,1,0),
+        (0,0,0,1,0,1,1,1,0,0),(0,0,0,0,0,0,1,0,1,0),(0,0,0,0,0,0,0,0,0,0))
 
-	if is_attacker:
-		return (attacker_wounds[dice][CD], attacker_capture[dice][CD])
-	else:
-		return (defender_wounds[dice][CD], defender_capture[dice][CD])
+    if CD <= -7:
+        CD = 0
+    elif CD >= -6 and CD <= -4:
+        CD = 1
+    elif CD >= -3 and CD <= -2:
+        CD = 2
+    elif CD == -1:
+        CD = 3
+    elif CD == 0:
+        CD = 4
+    elif CD == 1:
+        CD = 5
+    elif CD >= 2 and CD <= 3:
+        CD = 6
+    elif CD >= 4 and CD <= 6:
+        CD = 7
+    elif CD >= 7 and CD <= 10:
+        CD = 8
+    elif CD >= 11:
+        CD = 9
+
+    if is_attacker:
+        return (attacker_wounds[dice][CD], attacker_capture[dice][CD])
+    else:
+        return (defender_wounds[dice][CD], defender_capture[dice][CD])
 
 #Authored By Ben Cumber
 #military_units.dat info format
--- fitg/backend/actions/movement.py
+++ fitg/backend/actions/movement.py
@@ -4,55 +4,55 @@ from random import randint
 
 
 def move_environ(stack_id, environ_id):
-	session = Session()
+    session = Session()
 
-	newloc = session.query(Environ).filter_by(id = environ_id).one()
+    newloc = session.query(Environ).filter_by(id = environ_id).one()
 
-	if newloc == None:
-		raise Exception("No environ with that ID found")
-	oldloc = session.query(Stack).filter_by(id = stack_id).one().location
-	
-	if (int(newloc.id) / 10) != (int(oldloc.id) / 10):
-		raise Exception("Cannot move to non-adjacent environs")
+    if newloc == None:
+        raise Exception("No environ with that ID found")
+    oldloc = session.query(Stack).filter_by(id = stack_id).one().location
+    
+    if (int(newloc.id) / 10) != (int(oldloc.id) / 10):
+        raise Exception("Cannot move to non-adjacent environs")
 
-	# if moving to Orbit (environ id ends in '0') then PDB routines ?
+    # if moving to Orbit (environ id ends in '0') then PDB routines ?
 
-	moving_stack = session.query(Stack).filter_by(id = stack_id).one()
-	moving_stack.location = newloc
+    moving_stack = session.query(Stack).filter_by(id = stack_id).one()
+    moving_stack.location = newloc
 
-	session.add(moving_stack)
-	session.commit()
+    session.add(moving_stack)
+    session.commit()
 
 def merge_stack(src_id, des_id):
-	session = Session()
+    session = Session()
 
-	src_stack = session.query(Stack).filter_by(id = src_id).one()
-	des_stack = session.query(Stack).filter_by(id = des_id).one()
+    src_stack = session.query(Stack).filter_by(id = src_id).one()
+    des_stack = session.query(Stack).filter_by(id = des_id).one()
 
-	#assert isinstance(src_stack, Stack)
-	#assert isinstance(des_stack, Stack)
+    #assert isinstance(src_stack, Stack)
+    #assert isinstance(des_stack, Stack)
 
-	# check if stacks are same team? same location?
+    # check if stacks are same team? same location?
 
-	assert src_stack.location == des_stack.location
+    assert src_stack.location == des_stack.location
 
-	for character in src_stack.characters:
-		character.stack_id = des_id
-	for unit in src_stack.militaryunits:
-		unit.stack_id = des_id
+    for character in src_stack.characters:
+        character.stack_id = des_id
+    for unit in src_stack.militaryunits:
+        unit.stack_id = des_id
 
-	session.add(des_stack)
-	session.delete(src_stack)
-	session.commit()
+    session.add(des_stack)
+    session.delete(src_stack)
+    session.commit()
 
 if __name__ == "__main__":
-	loadDatabase()
+    loadDatabase()
 
-	#move_environ(1,'3110')
-	#move_environ(2,'3112')
+    #move_environ(1,'3110')
+    #move_environ(2,'3112')
 
-	session = Session()
-	print session.query(Stack).filter_by(id = 1).one().location
-	
-	merge_stack(1,2)
-	print session.query(Stack).filter_by(id = 2).one().characters
+    session = Session()
+    print session.query(Stack).filter_by(id = 1).one().location
+    
+    merge_stack(1,2)
+    print session.query(Stack).filter_by(id = 2).one().characters

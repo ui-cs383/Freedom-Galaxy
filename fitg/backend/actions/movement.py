bc467e7b0315405635b4e942a0da435bbb28796b
Justin Hall <hall5714@vandals.uidaho.edu>
2013-12-11 14:36:40 -0800

    fixed environ_move to properly calculate location based on adjacency

--- fitg/backend/actions/movement.py
+++ fitg/backend/actions/movement.py
@@ -23,7 +23,7 @@ def move(session, stack_id, environ_id):
     # Check if either are None
     if newloc is not None and oldloc is not None:
         # Check if they aren't adjacent
-        if (int(newloc.id) / 10) != (int(oldloc.id) / 10):
+        if oldloc.planet == newloc.planet and (newloc.location - 1 == oldloc or newloc.location + 1 == oldloc.location):
             return False, "Invalid Move"
     else:
         # One is None, exit

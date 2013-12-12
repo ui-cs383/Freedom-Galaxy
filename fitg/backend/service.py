d4cb357a910192972cf876fc9a3b977ce1e231b3
Jeff Crocker <crockj72@gmail.com>
2013-12-12 00:59:43 -0800

    added exposed combat

--- fitg/backend/service.py
+++ fitg/backend/service.py
@@ -229,17 +229,17 @@ class FreedomService(rpyc.Service):
 
             return self.response('move', request, result[0], result[1])
 
-    def exposed_combat(self, attacker_stack_id, defender_stack_id, options, validate_only=False):
+    def exposed_combat(self, atk_id, def_id, options, validate_only=False):
         """Start combat between an attacker and a defender.
 
         Move takes a stack_id and moves it to location_id. If stack_id is in an enviorn and location_id 
         is an adjacent enviorn, a environ based move is completed. If location_id is a different enviorn a space
         move is completed.
 
-        :param attacker_stack_id: The unique id of the attacking stack.
-        :type attacker_stack_id: int.
-        :param defender_stack_id: The unique id of the defending stack.
-        :type defender_stack_id: int.
+        :param atk_id: The unique id of the attacking stack.
+        :type atk_id: int.
+        :param def_id: The unique id of the defending stack.
+        :type def_id: int.
         :param options: A tuple of flags as options for combat.
         :type options: tuple.
         :param validate_only: If true the move will only be validated.
@@ -247,10 +247,23 @@ class FreedomService(rpyc.Service):
         :returns:  dict -- the results of the combat
         :raises: AssertionError
         """
-        assert isinstance(attacker_stack_id, int)
-        assert isinstance(defender_stack_id, int)
+        assert isinstance(atk_id, int)
+        assert isinstance(def_id, int)
+        with session_scope(self.orm) as session:
+            self.logger.info("requested combat outcome for attacker " + str(atk_id) + " and " + str(def_id))
+            request = locals()
+            atk_stack = session.query(Stack).filter_by(id = atk_id).one()
+            def_stack = session.query(Stack).filter_by(id = def_id).one()
+            try:
+                if not def_stack.units:
+                    result = self.actions.combat.char_combat(session, atk_id, def_id, options)
+                else:
+                    if atk_stack.units
+                        result = self.actions.combat.mil_combat(session, atk_id, def_id)
+            except AssertionError:
+                self.logger.warn("combat of " + str(atk_id) + " attacking " + str(def_if) + " failed"
 
-        self.logger.info("requested combat outcome for attacker " + str(attacker_stack_id) + " and " + str(defender_stack_id))
+            return self.response('combat ', request, result[0], result[1])
 
     def exposed_split_stack(self, stack_id, unit_id=None, character_id=None, validate_only=False):
         """Move a stack to a location.

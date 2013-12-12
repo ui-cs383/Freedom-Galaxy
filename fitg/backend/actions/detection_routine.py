2bbe37d181700d6f62dfe84cd0cf7744efe7fa18
Ben <cumb6959@vandals.uidaho.edu>
2013-12-11 18:55:00 -0800

    Detection Routine fixed

--- fitg/backend/actions/detection_routine.py
+++ fitg/backend/actions/detection_routine.py
@@ -8,30 +8,31 @@ from orm import *
     #The following detection table is exactly the same as the one from FitG with
     # E = -2, Dd = -1, D = 0, U = 1
     detection_table = ((-1, 0, 1, 1, 1, 1, 1, 1),
-                            (-1, 0, 0, 0, 1, 1, 1, 1),
-                            (-1, -1, 0, 0, 0, 1, 1, 1),
-                            (-2, -1, -1, 0, 0, 0, 1, 1),
-                            (-2, -1, -1, -1, 0, 0, 0, 1),
-                            (-2, -2, -2, -1, -1, 0, 0, 0))
+                       (-1, 0, 0, 0, 1, 1, 1, 1),
+                       (-1, -1, 0, 0, 0, 1, 1, 1),
+                       (-2, -1, -1, 0, 0, 0, 1, 1),
+                       (-2, -1, -1, -1, 0, 0, 0, 1),
+                       (-2, -2, -2, -1, -1, 0, 0, 0))
 # detection_table = (('Dd', 'D', 'U', 'U', 'U', 'U', 'U', 'U'),
 #                            ('Dd', 'D', 'D', 'D', 'U', 'U', 'U', 'U'),
 #                            ('Dd', 'Dd', 'D', 'D', 'D', 'U', 'U', 'U'),
 #                            ('E', 'Dd', 'Dd', 'D', 'D', 'D', 'U', 'U'),
 #                            ('E', 'Dd', 'Dd', 'Dd', 'D', 'D', 'D', 'U'),
 #                            ('E', 'E', 'E', 'Dd', 'Dd', 'D', 'D', 'D') )
-def detection_routine(session, Stackid, pdb_level):
+def detection_routine(session, stack_id, pdb_level):
     evasion_val = 0 #can be 0-7
     die_roll = 0    #can be 0-5
 
 
-    stack1 = session.query(Stack).filter_by(id = Stackid).one()
+    stack1 = session.query(Stack).filter_by(id = stack_id).one()
     characters = stack1.characters
     navigation_rating = characters[0].navigation
+    session.add(stack1)
     for char in characters:                         #Find the character in the stack
         if char.navigation > navigation_rating:     #with the highest navigation ratings
             navigation_rating = char.navigation
 
-    spaceship = stack1.Spaceship()
+    spaceship = stack1.spaceship()
 
     manuever_rating = spaceship.stat3
 
@@ -71,3 +72,20 @@ def detection_routine(session, Stackid, pdb_level):
     if(pdb_level == 0):                             #modifier for when pdb is 0
         if(result < 0):                             #if the result was Dd or E its just D.
             result = 0
+
+    # E = -2, Dd = -1, D = 0, U = 1
+    if(result == 1):
+        for character in stack1.characters:
+            character.detected = False
+    elif(result == 0):
+        for character in stack1.characters:
+            character.detected = True
+    elif(result == -1):
+        for character in stack1.characters:
+            character.detected = True
+            session.delete(stack1.spaceship())
+    else(result == -2):
+        for character in stack1.characters:
+            session.delete(character)
+        session.delete(stack1)
+    session.commit
